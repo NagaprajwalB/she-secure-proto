1c49e89eeead3a4841d96f39fc053e58903e9858
Nagaprajwal B <136093225+NagaprajwalB@users.noreply.github.com>
2025-05-18 10:44:15 +0530

    Update Meditation.tsx

--- src/pages/Meditation.tsx
+++ src/pages/Meditation.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import MeditationList from "@/components/meditation/MeditationList";
 
+
 const Meditation = () => {
   return (
     <>
